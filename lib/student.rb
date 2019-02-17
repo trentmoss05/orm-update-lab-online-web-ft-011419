b5dbfe3bdc9a903fe7e8f8867b392c197478bb46
Trent Moss <trentmoss05@gmail.com>
2019-02-17 04:44:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,6 +53,10 @@ class Student
     Student.new(result[0], result[1], result[2])
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 end
